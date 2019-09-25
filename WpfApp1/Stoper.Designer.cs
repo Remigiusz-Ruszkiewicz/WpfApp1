582e315a31889391425ac7094f3f6f801eff76cb
jagoda2309 <47605818+jagoda2309@users.noreply.github.com>
2019-09-25 21:45:07 +0200

    Add project files.

---
+++ WpfApp1/Stoper.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace WpfApp1
+{
+    partial class Stoper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LabelMin = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.LabelSec = new System.Windows.Forms.Label();
+            this.LabelCs = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LabelHr = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LabelMin
+            // 
+            this.LabelMin.AutoSize = true;
+            this.LabelMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LabelMin.Location = new System.Drawing.Point(224, 72);
+            this.LabelMin.Name = "LabelMin";
+            this.LabelMin.Size = new System.Drawing.Size(57, 39);
+            this.LabelMin.TabIndex = 0;
+            this.LabelMin.Text = "00";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(213, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(416, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // LabelSec
+            // 
+            this.LabelSec.AutoSize = true;
+            this.LabelSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LabelSec.Location = new System.Drawing.Point(320, 72);
+            this.LabelSec.Name = "LabelSec";
+            this.LabelSec.Size = new System.Drawing.Size(57, 39);
+            this.LabelSec.TabIndex = 4;
+            this.LabelSec.Text = "00";
+            // 
+            // LabelCs
+            // 
+            this.LabelCs.AutoSize = true;
+            this.LabelCs.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LabelCs.Location = new System.Drawing.Point(416, 72);
+            this.LabelCs.Name = "LabelCs";
+            this.LabelCs.Size = new System.Drawing.Size(57, 39);
+            this.LabelCs.TabIndex = 5;
+            this.LabelCs.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(191, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 39);
+            this.label1.TabIndex = 6;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(287, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 39);
+            this.label2.TabIndex = 7;
+            this.label2.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(383, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 39);
+            this.label3.TabIndex = 8;
+            this.label3.Text = ":";
+            // 
+            // LabelHr
+            // 
+            this.LabelHr.AutoSize = true;
+            this.LabelHr.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LabelHr.Location = new System.Drawing.Point(128, 72);
+            this.LabelHr.Name = "LabelHr";
+            this.LabelHr.Size = new System.Drawing.Size(57, 39);
+            this.LabelHr.TabIndex = 9;
+            this.LabelHr.Text = "00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(215, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 39);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Min";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(307, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 39);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Sec";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(409, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 39);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Ms";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(130, 23);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 39);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Hr";
+            // 
+            // Stoper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(584, 411);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LabelHr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LabelCs);
+            this.Controls.Add(this.LabelSec);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LabelMin);
+            this.Name = "Stoper";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stoper";
+            this.Load += new System.EventHandler(this.Stoper_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelMin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label LabelSec;
+        private System.Windows.Forms.Label LabelCs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LabelHr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ WpfApp1/Stoper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WpfApp1
+{
+    public partial class Stoper : Form
+    {
+        int timeCs, timeSec, timeMin,timeHr;
+        bool isActive;
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isActive = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            isActive = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isActive = false;
+            ResetTime();
+        }
+
+        private void ResetTime()
+        {
+            timeCs = 0;
+            timeSec = 0;
+            timeMin = 0;
+            timeHr = 0;
+        }
+        private void DrawTime()
+        {
+
+            LabelCs.Text = String.Format("{0:00}",timeCs);
+            LabelSec.Text = String.Format("{0:00}", timeSec);
+            LabelMin.Text = String.Format("{0:00}", timeMin);
+            LabelHr.Text = String.Format("{0:00}", timeHr);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isActive == true)
+            {
+                timeCs++;
+                if (timeCs >= 60)
+                {
+                    timeSec++;
+                    timeCs = 0;
+                    if (timeSec >= 60)
+                    {
+                        timeMin++;
+                        timeSec = 0;
+                        if (timeHr >=60)
+                        {
+                            timeHr++;
+                            timeMin = 0;
+                        }
+                    }
+                }
+            }
+            DrawTime();
+            
+        }
+
+       
+        public Stoper()
+        {
+            InitializeComponent();
+        }
+        private void Stoper_Load(object sender, EventArgs e)
+        {
+
+            ResetTime();
+            IsAccessible = false;
+        }
+    }
+}
