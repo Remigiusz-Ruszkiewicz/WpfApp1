0f2652a770a391be9e5cafb78b5d64d1312b6869
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-09-30 18:11:43 +0200

    adasd

--- WpfApp1/Minutnik.Designer.cs
+++ WpfApp1/Minutnik.Designer.cs
@@ -28,20 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.labelMin = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelSec = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.textBoxSec = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // labelMin
+            // 
+            this.labelMin.AutoSize = true;
+            this.labelMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelMin.Location = new System.Drawing.Point(214, 138);
+            this.labelMin.Name = "labelMin";
+            this.labelMin.Size = new System.Drawing.Size(57, 39);
+            this.labelMin.TabIndex = 0;
+            this.labelMin.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(277, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ":";
+            // 
+            // labelSec
+            // 
+            this.labelSec.AutoSize = true;
+            this.labelSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelSec.Location = new System.Drawing.Point(310, 138);
+            this.labelSec.Name = "labelSec";
+            this.labelSec.Size = new System.Drawing.Size(57, 39);
+            this.labelSec.TabIndex = 2;
+            this.labelSec.Text = "00";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(102, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Pauza";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(183, 341);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(284, 344);
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMin.TabIndex = 6;
+            this.textBoxMin.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBoxSec
+            // 
+            this.textBoxSec.Location = new System.Drawing.Point(400, 344);
+            this.textBoxSec.Name = "textBoxSec";
+            this.textBoxSec.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSec.TabIndex = 7;
+            this.textBoxSec.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(291, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Minuty";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(403, 313);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Sekundy";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
             // Minutnik
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 411);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxSec);
+            this.Controls.Add(this.textBoxMin);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelSec);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelMin);
             this.Name = "Minutnik";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Minutnik";
+            this.Load += new System.EventHandler(this.Minutnik_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelMin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelSec;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBoxMin;
+        private System.Windows.Forms.TextBox textBoxSec;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- WpfApp1/Minutnik.cs
+++ WpfApp1/Minutnik.cs
@@ -12,10 +12,116 @@ namespace WpfApp1
 {
     public partial class Minutnik : Form
     {
+        bool isActive;
+        public int min()
+        {
+            //int min = int.Parse(textBoxMin.Text);           
+            //int min = Convert.ToInt32(textBoxMin.Text);
+
+            int min;
+
+            if (int.TryParse(textBoxMin.Text, out min))
+            {
+                //parsing successful 
+                min = int.Parse(textBoxMin.Text);
+
+            }
+            else
+            {
+                //parsing failed. 
+            }      
+   
+            return min;
+        }
+        public int sec()
+        {
+            //int sec = int.Parse(textBoxSec.Text);
+
+            int sec;
+
+            if (int.TryParse(textBoxSec.Text, out sec))
+            {
+                //parsing successful 
+                sec = int.Parse(textBoxSec.Text);
+
+            }
+            else
+            {
+                //parsing failed. 
+            }
+
+            return sec;
+        }
+        public int mi()
+        {
+            return min();
+        }
+        public int se()
+        {
+            return sec();
+        }
         public Minutnik()
         {
             InitializeComponent();
             
+        }
+
+        private void Minutnik_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void DrawTime()
+        {
+            
+            //labelMin.Text = String.Format("{0:00}", min());
+            //labelSec.Text = String.Format("{0:00}", sec());
+            labelMin.Text = String.Format("{0:00}", mi());
+            labelSec.Text = String.Format("{0:00}", se());
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DrawTime();
+            isActive = true;
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            isActive = false;
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            int min = int.Parse(textBoxMin.Text);
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            int sec = int.Parse(textBoxSec.Text);
+        }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            isActive = false;
+
+            
+        }
+      
+        public void Timer1_Tick(object sender, EventArgs e)
+        {
+            var m = min();
+            var s = sec();
+
+            if (isActive == true)
+            {
+                s--;
+                if (s == 0)
+                {
+                    m--;
+                    s = 60;
+                }
+            }
+            
+            DrawTime();
         }
     }
 }
